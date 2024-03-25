e59576bb4fa42d58cd063f4659264e18730e3fcc
Ewerton Lopes <85997073+oewertonlopes@users.noreply.github.com>
2024-03-25 14:07:47 -0300

    Update js.js

--- BarraDeProgresso/js.js
+++ BarraDeProgresso/js.js
@@ -3,7 +3,10 @@ const input = document.querySelector("input");
 const numero = document.querySelector(".numero");
 
 const alterarProgresso = () => {
-  const valor = input.value;
-  progresso.setAttribute("style", "width: " + valor + "%");
+  const valor = parseInt(progresso.style.width, 10);
+  input.value = valor;
   numero.textContent = valor;
 };
+
+// Adicionar um event listener para detectar mudanças na barra de progresso
+progresso.addEventListener("transitionend", alterarProgresso);
